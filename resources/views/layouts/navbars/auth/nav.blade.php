8c2c31bf04d7c674d43438f6db92ddc43bc90828
johnkenneth24 <kengerero@yahoo.com>
2023-07-12 14:28:32 +0800

    first commit, fixed log in

---
+++ resources/views/layouts/navbars/auth/nav.blade.php
@@ -0,0 +1,33 @@
+<!-- Navbar -->
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
+    <div class="container-fluid py-1 px-3">
+        <nav aria-label="breadcrumb">
+            <h6 class="font-weight-bolder mb-0 text-capitalize">{{ str_replace('-', ' ', Request::path()) }}</h6>
+        </nav>
+        <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4 d-flex justify-content-end" id="navbar">
+
+            <ul class="navbar-nav  justify-content-end">
+            <li class="nav-item d-flex align-items-center">
+                <a href="{{ url('/logout')}}" class="nav-link text-body font-weight-bold px-0">
+                    <i class="fas fa-sign-out-alt"></i>
+                    <span class="d-sm-inline d-none">Sign Out</span>
+                </a>
+            </li>
+            <li class="nav-item d-xl-none ps-3 d-flex align-items-center">
+                <a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+                <div class="sidenav-toggler-inner">
+                    <i class="sidenav-toggler-line"></i>
+                    <i class="sidenav-toggler-line"></i>
+                    <i class="sidenav-toggler-line"></i>
+                </div>
+                </a>
+            </li>
+
+
+                </ul>
+            </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+<!-- End Navbar -->
---
+++ resources/views/session/login-session.blade.php
@@ -0,0 +1,71 @@
+@extends('layouts.user_type.guest')
+
+@push('links')
+    <style>
+        .background-login{
+            height: 100vh;
+            width: 100vw;;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+            background-image: url("{{ asset('assets/img/pnp-bg.jpg') }}");
+            background-repeat: no-repeat;
+            background-size: cover;
+            background-position: center;
+        }
+
+        .card-login{
+            width: 350px;
+            border: solid 1px #000000;
+            padding: 20px;
+            border-radius: 8px;
+            /* From https://css.glass */
+            background: rgba(255, 255, 255, 0.19);
+            border-radius: 16px;
+            box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
+            backdrop-filter: blur(5px);
+            -webkit-backdrop-filter: blur(5px);
+            border: 1px solid rgba(255, 255, 255, 0.3);
+        }
+        .login-img-logo img{
+            text-align: center;
+        }
+
+    </style>
+@endpush
+
+@section('content')
+<div class="background-login">
+    <div class="login-img-logo mb-2">
+        <img src="{{ asset('assets/img/pnplogo.png') }}"  height="120">
+        <img src="{{ asset('assets/img/bmps.png') }}" height="160">
+    </div>
+    <div class="card-login">
+        <h4 class="text-center text-nowrap mb-0"><b>FILE MANAGEMENT SYSTEM</b></h4>
+        <h3 class="text-center">BULAN MPS</h3>
+        <form role="form" method="POST" action="/session">
+          @csrf
+          <label>Username</label>
+          <div class="mb-3">
+            <input type="text" class="form-control" name="email" id="email" placeholder="Username" value="" aria-label="Email" aria-describedby="email-addon">
+            @error('email')
+              <p class="text-danger text-xs mt-2">{{ $message }}</p>
+            @enderror
+          </div>
+          <label>Password</label>
+          <div class="mb-3">
+            <input type="password" class="form-control" name="password" id="password" placeholder="Password" value="" aria-label="Password" aria-describedby="password-addon">
+            @error('password')
+              <p class="text-danger text-xs mt-2">{{ $message }}</p>
+            @enderror
+          </div>
+          <div class="text-center">
+            <button type="submit" class="btn bg-gradient-info w-100 mt-2 mb-0">Sign in</button>
+          </div>
+        </form>
+      </div>
+</div>
+
+
+@endsection
